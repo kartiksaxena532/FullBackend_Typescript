f9639c54150959278c1405e31a1cc5e69a63052f
Kartik Saxena <karrtiksaxena@gmail.com>
2024-04-19 00:13:54 +0530

    routes added

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,18 @@ import express from "express";
 
 const app = express();
 
+app.get("/", (req, res,next) => {
 
+    res.json({ message: "Hello kartik welcome to backend" });
 
+}
+)
+
+app.get("/kartik", (req, res,next) => {
+
+    res.json({ message: "Hello kartik welcome" });
+
+}
+)
 
 export default app;
